fc471682dca0a2d6ae5b04ea5f66f9f6d4cb120f
Monaa <fherbert@gmail.com>
2015-02-05 11:39:38 +0100

    MODIFS

--- header.php
+++ header.php
@@ -5,12 +5,15 @@
       <td width="172" height="166"><a href="/"><img src="Images/logo.png"  /></a></td>
       <td width="941" align="left" valign="middle"><p><strong><font size="10"><em>Cercles</em></font></strong></p>
       <p><strong><em>Réseau Social de proximité au service des centres d'intérets</em></strong></p></td>
-      <td width="77" align="right" valign="bottom">      
+      <td width="77" align="right" valign="bottom">
       <a href="/index.php?action=index_identifies"><input type="button" name="s'identifier" id="s'identifier" value="S'identifier" >            
-      </a></td>
-      <td width="70" align="right" valign="bottom">
-      <a href="/index.php?action=inscription"><input type="button" name="s'inscrire" id="s'inscrire" value="S'inscrire">
-      </a></td>
+      </a>
+      </td>
+      <td width="77" align="left" valign="bottom">
+      <a href="/index.php?action=inscription">
+          <input type="button" name="s'inscrire" id="s'inscrire" value="S'inscrire">
+      </a>
+      </td>	
     </tr>
   </tbody>
 </table>
--- index_identifies.php
+++ index_identifies.php
@@ -12,30 +12,24 @@ body,td,th {
 </head>
 
 <body>
-<p align="left"><a href="index.html"><img src="Images/logo.jpg" width="172" height="162" alt=""/></a></p>
-<p align="right">
-  <a href="index.html"><input type="button" name="se déconnecter" id="se déconnecter" value="Se déconnecter"></a>
-  <input type="button" name="Paramètres" id="Paramètres" value="[NOM UTILISATEUR]">
-</p>
-<hr>
-<p><strong>Trouver un cercle dans votre ville</strong></p>
+<p align="left"><strong>Trouver un cercle dans votre ville</strong></p>
 <p>
   <input name="recherche cercle" type="search" id="recherche cercle" autocomplete="off" value="Saisir la ville, le départemet, la région, le pays" size="70">
   <a href="résultat recherche cercle.html">
   <input type="button" name="rechercher" id="rechercher" value="Rechercher">
 </a></p>
-<p>&nbsp;</p>
-<h2><strong>Mes cercles&nbsp;: </strong></h2>
+<p></p>
+<h2><strong>Mes cercles : </strong></h2>
 <p>[VIGNETTE CERCLE]</p>
-<p>&nbsp;</p>
-<h2><strong>Derni&egrave;re  cr&eacute;ation de cercle&nbsp;: </strong></h2>
+<p></p>
+<h2><strong>Dernière  création de cercle : </strong></h2>
 <p>[VIGNETTE CERCLE]</p>
-<p>&nbsp;</p>
-<h2><strong>Derni&egrave;re  cr&eacute;ation de cercle&nbsp;près de chez moi : </strong></h2>
+<p></p>
+<h2><strong>Dernière  création de cercle près de chez moi : </strong></h2>
 <p>[VIGNETTE CERCLE] (10 km)</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<h2><strong>Les cat&eacute;gories  de cercles</strong></h2>
+<p></p>
+<p></p>
+<h2><strong>Les catégories  de cercles</strong></h2>
 <ul>
   <li>Activtés mauelles</li>
   <li>Activités artistiques</li>
@@ -46,7 +40,7 @@ body,td,th {
   <li>Famille</li>
   <li>Autres</li>
 </ul>
-<p>&nbsp;</p>
+<p></p>
 <p><a href="mailto:Contact@XXX.com">Contact</a></p>
 </body>
 </html>
