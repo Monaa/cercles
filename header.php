6c11614c4b299eeb8ec1d33bb82aa6fcfc7e3264
davout <david.francois@webflows.fr>
2015-02-05 23:14:30 +0100

    WIP

--- header.php
+++ header.php
@@ -2,15 +2,15 @@
 <table width="100%" border="0">
   <tbody>
     <tr>
-      <td width="172" height="166" align="center"><a href="/"><img src="Images/logo.png"  /></a></td>
+      <td width="172" height="166" align="center"><a href="/"><img src="images/logo.png"  /></a></td>
       <td width="941" align="left" valign="middle"><p><strong><font size="10"><em>Cercles</em></font></strong></p>
       <p><strong><em>Réseau Social de proximité au service des centres d'intérets</em></strong></p></td>
       <td width="77" align="right" valign="bottom">
-      <a href="/index.php?action=index_identifies"><input type="button" name="s'identifier" id="s'identifier" value="S'identifier" >            
+      <a href="/?action=index_identifies"><input type="button" name="s'identifier" id="s'identifier" value="S'identifier" >            
       </a>
       </td>
       <td width="77" align="left" valign="bottom">
-      <a href="/index.php?action=inscription">
+      <a href="/?action=inscription">
           <input type="button" name="s'inscrire" id="s'inscrire" value="S'inscrire">
       </a>
       </td>	
--- index.php
+++ index.php
@@ -1,39 +1,37 @@
 <!doctype html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Index</title>
-		<style type="text/css">
-			body,td,th {
-				font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, sans-serif;
-				color: #993365;
-			}
-		</style>
-        
-        <link href="/styles.css" type="text/css" rel="stylesheet" />
-	</head>
+  <head>
+    <meta charset="utf-8" />
+    <title>Index</title>
+    <style type="text/css">
+      body,td,th {
+        font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, sans-serif;
+        color: #993365;
+      }
+    </style>
 
-	<body>
-		<?php include('header.php'); ?>
+    <link href="/styles.css" type="text/css" rel="stylesheet" />
+  </head>
 
+  <body>
+    <?php include('header.php'); ?>
+    
+    <div id="background_contenu">
 
-		<div id="background_contenu">
-			<div id="contenu">
-				<?php 
-		 			if (isset($_GET['action'])) {
-		    			include($_GET['action'] . '.php');
-		  			}
-		  			else {
-						include('home.php');
-					}
-				?>
-        	</div>
-        </div>
-        
-       <?php include('footer.php'); ?>
-       
-       		<div id="footer">
-            </div>
-       
-	</body>
+    <div id="contenu">
+      <?php 
+        if (isset($_GET['action'])) {
+          include($_GET['action'] . '.php');
+        }
+        else {
+          include('home.php');
+        }
+      ?>
+    </div>
+
+    <div id="footer"> 
+      <?php include('footer.php'); ?>
+    </div>
+
+  </body>
 </html>
