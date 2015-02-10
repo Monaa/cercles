7a3ec9c168714579375d267508f6064b72f91a53
davout <david.francois@webflows.fr>
2015-02-10 11:59:41 +0100

    Nouvelle manière de se connecter à la DB

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
   $db_name  = "cercles";
 
   //connection to the database
+  // Je laisse la première manière de façon à ce que tout ne se mette
+  // pas à planter d'un coup. A enlever dès que tous les appels à la DB auront été changés
   $mysql = mysqli_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
   mysqli_select_db($mysql, $db_name) or die("Could not select DB");
   mysqli_query($mysql, "SET NAMES utf8");
+
+  // Connection au serveur, d'une autre manière
+  $dns = 'mysql:host=fr.anco.is;dbname=cercles';
+  $db  = new PDO( $dns, $username, $password );
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 <!doctype html>
 <html>
@@ -16,7 +23,7 @@
     <title>Index</title>
     
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link href="/styles.css" type="text/css" rel="stylesheet" 
+    <link href="/styles.css" type="text/css" rel="stylesheet">
   </head>
 
   <body>
