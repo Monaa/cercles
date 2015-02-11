70cdd1540ff901220bc706cac1969b8a209213e6
Monaa <fherbert@gmail.com>
2015-02-11 16:56:58 +0100

    reherche cercle

--- recherche_cercle.php
+++ recherche_cercle.php
@@ -2,4 +2,12 @@
 
   <input name="recherche cercle" type="search" id="recherche cercle" autocomplete="off" value="Saisir la ville, le départemet, la région, le pays" size="70">
 
+
+<?php
+  if (!isset($villes)) {
+    $res = mysqli_query($mysql, "SELECT id, nom FROM villes.");
+    $dernier_cercle = mysqli_fetch_array($res);
+  }
+
+
 	<a  class="btn btn-default" href="/?action=resultat_recherche_cercle">Rechercher</a>   
