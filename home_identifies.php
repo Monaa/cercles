81ff9887b880ec9eaad0790681c67eb786cd43e6
davout <david.francois@webflows.fr>
2015-02-13 00:23:39 +0100

    Pouet

--- home_identifies.php
+++ home_identifies.php
@@ -2,16 +2,12 @@
 
 <h2>Mes cercles </h2>
 <?php include ('mes_cercles.php'); ?>
-<div class="clear">
 <?php vignettes_pour_cercles_par_utilisateur($db, $_SESSION['id_utilisateur']); ?>
-</div>
 
 
-<div>
 <h2>Dernière création de cercle</h2>
 <?php include('vignette_derniere_creation_de_cercle.php'); ?>
 
 <h2>Dernière  création de cercle près de chez moi</h2>
 <p>[VIGNETTE CERCLE] (10 km)</p>
-</div>
  
