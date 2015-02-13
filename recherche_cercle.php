b73d0b78aea46d76705d0df99035826f83c12247
Monaa <fherbert@gmail.com>
2015-02-13 08:57:05 +0100

    modification du style de la zone de saisie de recherche_cercle.php

--- recherche_cercle.php
+++ recherche_cercle.php
@@ -1,8 +1,19 @@
 <h2>Trouver un cercle près de chez vous</h2>
 
-  <input name="recherche cercle" type="search" id="recherche cercle" autocomplete="off" value="Saisir la ville, le départemet, la région, le pays" size="70">
+<form method="post" action="/index.php?action=recherche_cercle">
+  				<!-- L'action est facultative quand le formulaire est traité par le même script PHP -->
+  
+  
+    <div class="form-group">
+		<label for="recherche cercle"></label>
+       <input id="recherche cercle" name)"pseudo" class="form-control" autocomplete="off" value="Saisir la ville, le départemet, la région, le pays" />
+       
+       
+	</div>
 
 
-
-	<a  class="btn btn-default" href="/?action=resultat_recherche_cercle">Rechercher</a>   
-	
+  	<div class="form-group">
+      <input type="submit" value="Rechercher" class="btn default" />
+  	</div>
+  
+</form>	
