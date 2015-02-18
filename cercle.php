9c8141fdb0c97ee16b738b2c42b564d581c8df24
davout <david.francois@webflows.fr>
2015-02-18 15:00:04 +0100

    Have fun

--- cercle.php
+++ cercle.php
@@ -1,51 +1,40 @@
-<div id="cercle">
-
 <?php
   if (isset($_GET['id_cercle'])) {
-   $req = $db->prepare ('SELECT * FROM cercles where id = :id');
 
-    $req->execute(array(
-      ':id' =>  	$_GET['id_cercle']
-    ));
-              
-         while ($ce = $req->fetch()) { 
-?> 
-  							<p>
-                            <h1> <?php echo($ce{'nom_cercle'}); ?></h1>
-  							<h2>[VILLE] - [DEPARTEMENT]</h2>                            
-  							<p>Description </p>
-							<p><?php echo($ce{'commentaire'}); ?></p>
-							<p>Date de création</p>
-							<p><?php echo($ce{'creation'}); ?>
-							<?php 					
- }?>
-				
-  <?php                
-   $req = $db->prepare ('
-			SELECT COUNT(*) FROM cercles
-			INNER JOIN cercles_utilisateurs ON cercles.
-			cercles.id = id_cercle
-         	where id=3			
-			');
+    $req = $db->prepare('SELECT * FROM cercles where id = :id');
+    $req->execute(array( ':id' => $_GET['id_cercle'] ));
+  
+    if ($ce = $req->fetch()) { ?> 
+      <div class="cercle" id="cercle-<?php echo($ce{'id'}) ?>">
+        <h1> <?php echo($ce{'nom_cercle'}); ?></h1>
+        <h2>[VILLE] - [DEPARTEMENT]</h2>                            
+        <p>Description </p>
+        <p><?php echo($ce{'commentaire'}); ?></p>
+        <p>Date de création</p>
+        <p><?php echo($ce{'creation'}); ?></p>
+  
+        <?php $req_count = $db->prepare('SELECT COUNT(*) AS cnt FROM cercles_utilisateurs WHERE id_cercle = :id_cercle');
+  
+        $req_count->execute(array( ':id_cercle' => $_GET['id_cercle'] ));
+  
+        $ce_count = $req_count->fetch(); ?>
+        <p>Nombre de membres : <?php echo($ce_count{'cnt'}); ?></p>
+        <h2>Membres du cercle </h2>
+  
+        <p><img src="images/@.png" />Inviter des membres</p>
+  
+        <ul>  
+          <li>Responsable du cercle </li>
+          <li>[AVATAR] [PRENOM] [1ERE LETTRE DU NOM]</li>
+        </ul>
+      </div>
 
-  $req->execute();
-              
-         while ($ce = $req->fetch()) { 
-?> 
-							
-  						    <p>Nombre de membres : <?php echo($ce{'COUNT(*)'}); ?></p>
-						  	<h2>Membres du cercle </h2>
+    <?php } else { ?>
+      <div>Cercle introuvable !</div>
+    <?php } ?>
 
-  <?php } }?>                   		  
-							  
+  <?php } else { ?>
+    <div>Paramètre manquant dans l'URL</div>
+  <?php } 
+?>
 
-<p></p>
-    
- <p><img src="images/@.png"	/>  Inviter des membres</p>
- 
-<ul>  
-    <li>Responsable du cercle </li>
-    <li>[AVATAR] [PRENOM] [1ERE LETTRE DU NOM]</li>
-</ul>
-
-</div> 
